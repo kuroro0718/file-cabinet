8ec6434113ba9c0cd02dcafc1f538afc2e92c8a1
Alex Liang <u9211011@gmail.com>
2016-03-16 15:29:46 +0800

    Change root of application for authenticated user

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,6 +1,6 @@
 class DocsController < ApplicationController
   before_action :find_doc, only: [:show, :edit, :update, :destroy]
-  
+    
   def index
     @docs = Doc.where(user_id: current_user)
   end 
